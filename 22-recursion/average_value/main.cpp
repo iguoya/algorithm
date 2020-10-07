858a96eeb18531b6bcc9ac3461b5fed9167fcaa9
tiger <375478250@qq.com>
2020-10-07 14:15:32 +0800

    全排列递归算法

--- 22-recursion/average_value/main.cpp
+++ 22-recursion/average_value/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <random>
 #include <ctime>
+#include <list>
+
 using namespace std;
 
 //int average_value()
@@ -28,5 +30,40 @@ int main() {
 //    uniform_int_distribution<int> u(1, 10000);
     cout<< summation(10)<<endl;
     cout<< factorial(10)<<endl;
+    list<int> sample = {1, 2, 3, 4, 5, 6, 7};
+    sample.remove(5);
+    for(auto m: sample) {
+        cout<<m<<"\t";
+    }
+    cout<<endl;
+
+
+    auto it = sample.begin();
+    for(auto i=0; i != sample.size(); ++i) {
+
+        if (i == 2 || i == 3) {
+            auto tmp = it;
+            ++it;
+            sample.erase(it);
+        } else {
+            ++it;
+        }
+
+    }
+
+
+//            auto tmp_it = ++it;
+
+//            it = tmp_it;
+//            cout<<"ddd";
+//            break;
+
+
+    for(auto m: sample) {
+        cout<<m<<"\t";
+    }
+
+    cout<<endl;
+
     return 0;
 }
---
+++ 22-recursion/permutation/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+
+template <typename T>
+void permutation(list<T> sample, vector<T> route, vector<vector<T>> &result) {
+    if(sample.size() == 0) {
+        result.push_back(route);
+    } else {
+        for(auto it = sample.begin(); it != sample.end(); ++it) {
+            route.push_back(*it);
+            list<T> se_sample(sample);
+            se_sample.remove(*it);
+            permutation(se_sample, route, result);
+            route.pop_back();
+        }
+    }
+}
+
+template <typename T>
+void show(vector<vector<T>> &result) {
+    for(const auto &route: result) {
+        for(const auto &node: route) {
+            cout<<node<<"\t";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    vector<vector<int>> result;
+    list<int> sample;
+    vector<int> route;
+
+    for(int i = 1; i < 5; ++i) {
+        sample.push_back(i);
+    }
+    permutation(sample, route, result);
+    show(result);
+//    result.erase();
+//    cout<<"is empty ??"<<endl;
+//    show(result);
+//    cout<<"empty!!!"<<endl;
+
+
+    vector<vector<string>> result2;
+    list<string> sample2 = {"I", "love", "china's", "history"};
+    vector<string> route2;
+    permutation(sample2, route2, result2);
+    show(result2);
+
+
+    return 0;
+}
